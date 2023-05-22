b48e0073568604f2f88e8623bb88d49d4878a2d5
xorvet <cypherx702@gmail.com>
2023-05-22 22:52:56 +0530

    cute

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 export const DATA = {};
 
+function fetchHTML(path) {
+  return fetch(path).then((doc) => doc.text());
+};
+
 export function Component(id, path) {
-  fetch(path)
-    .then((doc) => doc.text())
-    .then((html) => {
-      const htmldoc = (new DOMParser()).parseFromString(html, "text/html");
-      document.querySelector(id).appendChild(htmldoc.body);
-    });
+  fetchHTML(path).then((html) => {
+    const htmldoc = (new DOMParser()).parseFromString(html, "text/html"); 
+    document.querySelector(id).appendChild(htmldoc.body);
+  }); 
+};
+
+export function Route(id,paths) {
+  const routdiv = document.querySelector(id);
+  
+  function Routing() {
+    const hash = (window.location.hash).slice(1);
+    if (Object.keys(paths).includes(hash)) {
+      fetchHTML(paths[hash]).then((html) => {
+        routdiv.innerHTML = html;
+      });
+    } else {
+      routdiv.innerHTML = '';
+    }
+  };
+  Routing();
+
+  window.addEventListener('hashchange', () => {
+    Routing();
+  });
 };
