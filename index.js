d77d4defddb012f0abce51c3172f21779fb0d426
xorvet <cypherx702@gmail.com>
2023-05-23 03:28:20 +0530

    cute

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 export const DATA = {};
 
 function fetchHTML(path) {
-  return fetch(path).then((doc) => doc.text());
+  return fetch(path)
+    .then((doc) => doc.text())
+    .then((html) => {
+      const doc = (new DOMParser()).parseFromString(html, "text/html").body.children;
+      const script = document.createElement('script');
+      script.innerText = doc[1].innerText;
+      return [doc[0], script];
+    });
 };
 
 export function Component(id, path) {
-  fetchHTML(path).then((html) => {
-    const htmldoc = (new DOMParser()).parseFromString(html, "text/html"); 
-    document.querySelector(id).appendChild(htmldoc.body);
-  }); 
+  fetchHTML(path).then((kid) => {
+    document.querySelector(id).append(kid[0], kid[1]);
+  });
 };
 
-export function Route(id,paths) {
+export function Route(id, paths) {
   const routdiv = document.querySelector(id);
-  
+
   function Routing() {
     const hash = (window.location.hash).slice(1);
     if (Object.keys(paths).includes(hash)) {
-      fetchHTML(paths[hash]).then((html) => {
-        routdiv.innerHTML = html;
+      fetchHTML(paths[hash]).then((kid) => {
+        routdiv.innerHTML = '';
+        routdiv.append(kid[0], kid[1]);
       });
     } else {
       routdiv.innerHTML = '';
